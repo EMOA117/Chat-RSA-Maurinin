dbe2f3d95f0cd44e23eb5c556d730e678389b146
EMOA117 <emontesdeocaa1900@alumno.ipn.mx>
2021-11-01 13:08:34 -0600

    primera versión de cifrado

--- public/chat.js
+++ public/chat.js
@@ -27,6 +27,16 @@ btn.addEventListener('click', function() {
     output.innerHTML += `<p>
     <strong>${username.value}(Mensaje descifrado)</strong>: ${message.value}
   </p>`;
+  //comienza el cifrado
+  var tamprimo = new int; //2, 3, 4 etc
+   var p = new BigInt;
+   var q = new BigInt;
+   var n = new BigInt;
+   var fi = new BigInt;
+   var e = new BigInt;
+   var d = new BigInt;
+
+
   socket.emit('chat:message', {
     message: message.value,
     username: username.value
@@ -48,3 +58,89 @@ socket.on('chat:message', function(data) {
 socket.on('chat:typing', function(data) {
   actions.innerHTML =  `<p><em>${data} está escribiendo un mensaje...</em></p>`
 });
+
+function generarPrimos(){
+    
+  p = new BigInt(tamprimo, 10, new Random());
+  
+  do q = new BigInt(tamprimo, 10, new Random());
+   while(q.compareTo(p)==0); 
+
+}
+
+function generarClaves(){
+  /*
+  Recordar que n = p*q
+  fi = (p-1)*(q-1)
+  */
+  
+  n = p.multiply(q);
+  
+  //(p-1)
+  fi = p.subtract(BigInt.valueOf(1));
+  
+  fi = fi.multiply(q.subtract(BigInt.valueOf(1)));
+  
+  /*
+  como calculabamos a e
+  
+  e debe de ser un coprimo de n de tal que
+  1<e<fi(n)
+  */
+  
+  do e = new BigInt(2*tamprimo, new Random());
+  while((e.compareTo(fi) != -1) || (e.gcd(fi).compareTo(BigInteger.valueOf(1)) != 0));
+  
+  //calcular a d = e ^ 1 mod fi   inverso multiplicativo de e
+  
+  d = e.modInverse(fi);
+
+  
+}
+
+function cifrar(mensaje){
+        
+  var i = new int;
+  var temp = new byte[1];
+  var digitos = mensaje.getBytes();
+  
+  var bigdigitos = new BigInt[digitos.length];
+  
+  for(i = 0; i < bigdigitos.length; i++){
+      temp[0] = digitos[i];
+      bigdigitos[i] = new BigInt(temp);
+  }
+  
+  var cifrado = new BigInt[bigdigitos.length];
+  
+  for(i = 0; i < bigdigitos.length; i++){
+      //formula
+      // c = M ^ e mod n
+      cifrado[i] = bigdigitos[i].modPow(e, n);
+  }
+  
+  return cifrado;
+}
+
+//desciframos con clave privada
+// d n
+
+function descifrar(cifrado){
+  
+  var descifrado = new BigInt[cifrado.length];
+  
+  //vamos a descifrar con la formula
+  // Md = C ^d mod n
+  
+  for( var j = 0; j < descifrado.length; j++){
+      descifrado[j] = cifrado[j].modPow(d, n);
+  }
+  
+  var charArray = new char[descifrado.length];
+  
+  for( var i = 0; i < charArray.length; i++){
+      charArray[j] = (char)(descifrado[j].intValue());
+  }
+  
+  return (new String(charArray));
+}
